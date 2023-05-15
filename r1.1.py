195fcd6f19109b11dd62af17710c5117324f0f5d
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2023-05-15 22:03:50 +0800

    fix r1.1 只处理ts和js文件, 避免读取错误格式文件

--- r1.1.py
+++ r1.1.py
@@ -8,6 +8,7 @@
 import os
 import re
 
+
 def replace_hyphen_in_imports(code: str) -> str:
     def replacer(match):
         return match.group(0).replace('-', '_')
@@ -18,19 +19,21 @@ def replace_hyphen_in_imports(code: str) -> str:
 # print(replace_hyphen_in_imports(s))
 
 
-def replace_string_in_files(root_path):
+def replace_string_in_ts(root_path):
     for dirpath, dirnames, filenames in os.walk(root_path):
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
-            with open(filepath, 'r') as f:
-                content = f.read()
-            # content = re.sub(r'(@polkadot\/\w+)-', r'\1_', content)
-            content = replace_hyphen_in_imports(code=content)
-            # content = re.sub(r'import type { Observable } from \'rxjs\';', repl, content)
-            with open(filepath, 'w') as f:
-                f.write(content)
+            print('process# '+filepath)
+            if filepath.endswith('.js') or filepath.endswith('.ts'):
+                with open(filepath, 'r') as f:
+                    content = f.read()
+
+                content = replace_hyphen_in_imports(code=content)
+                with open(filepath, 'w') as f:
+                    f.write(content)
 
 
 if __name__ == '__main__':
     root_path = './packages'  # 当前路径
-    replace_string_in_files(root_path)
+    replace_string_in_ts(root_path)
+
