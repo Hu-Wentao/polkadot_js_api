af43f8ecd743393d4b82071a649219f871416634
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2023-05-15 22:55:31 +0800

    fix 2.1 处理异常情况

--- r2.1.py
+++ r2.1.py
@@ -7,6 +7,7 @@ import shutil
 # 获取当前工作目录
 current_dir = os.getcwd()
 
+
 def find_packages(path):
     """
     从2.0复制得到; 用于查询路径下所有包含package.json的文件夹;返回绝对路径list[str];
@@ -21,9 +22,13 @@ def find_packages(path):
     return package_directories
 
 # 遍历当前目录以及子目录
-def move_src_to_lib(str:root='./packages'):
+
+
+def move_src_to_lib(root: str = './packages'):
     # 获取 src 文件夹的路径
     src_path = os.path.join(root, 'src')
+    if not os.path.exists(src_path):
+        return
     # 获取 lib 文件夹的路径
     lib_path = os.path.join(root, 'lib', 'src')
     # 如果 lib 文件夹已经存在，则删除其中所有内容
@@ -32,7 +37,9 @@ def move_src_to_lib(str:root='./packages'):
     # 将 src 文件夹移动到 lib 文件夹中
     shutil.move(src_path, lib_path)
 
+
 if __name__ == '__main__':
     path = './packages'
     for root in find_packages(path):
+        print("root {}".format(root))
         move_src_to_lib(root=root)
