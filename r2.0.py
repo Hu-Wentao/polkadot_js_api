22419599f603484d8abae69a7e710fa72e11f35a
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2023-05-15 22:36:43 +0800

    fix 2.0 使用老sdk创建dart项目

--- r2.0.py
+++ r2.0.py
@@ -15,21 +15,24 @@ import os
 def find_package_json(path):
     for root, dirs, files in os.walk(path):
         if "package.json" in files:
+            # print("root {}, dirs {}, files {}".format(root, dirs, files))
             return root
     return None
 
 
-def execute_command(path, cmd):
+def execute_command(path):
     os.chdir(path)
-    os.system(cmd)
-    # os.system("/Users/huwentao/fvm/versions/1.22.6/bin/dart create . package --force")
+    base = os.path.basename(path)
+    print("path# {}, base# {} ".format(path, base))
+    os.system("/Users/huwentao/fvm/versions/1.22.6/bin/dart create " +
+              base+" --force -t package-simple")
 
 
 if __name__ == "__main__":
     # dir = os.getcwd()
     dir = './packages'
-    cmd = "/Users/huwentao/fvm/versions/1.22.6/bin/dart create . package --force"
     for root, dirs, files in os.walk(dir):
         package_dir = find_package_json(root)
         if package_dir:
-            execute_command(package_dir, cmd=cmd)
+            # print("root {}, dirs {}, files {}".format(root, dirs, files))
+            execute_command(package_dir)
